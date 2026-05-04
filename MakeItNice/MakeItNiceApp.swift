e8d87920a7645dc38a86a50edbe07412b44b721a
Mohammad Darmousa <mdarmousa1@gmail.com>
2026-05-05 03:23:20 +0400

    Initial Commit

---
+++ MakeItNice/MakeItNiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItNiceApp.swift
+//  MakeItNice
+//
+//  Created by Mohammad Darmousa on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MakeItNiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
